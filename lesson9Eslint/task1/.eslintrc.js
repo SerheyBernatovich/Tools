424aa7a177c52e0d36c94853ef5ddc76b283ffc1
SerheyBernatovich <sec1982@ukr.net>
2021-12-21 15:43:16 +0200

    add

--- lesson9Eslint/task1/.eslintrc.js
+++ lesson9Eslint/task1/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
+  extends: 'eslint-config-airbnb-base',
+  rules: {
+    'no-console': 'warn',
+  },
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
   globals: {
     document: true,
     alert: true,
